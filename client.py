c5f3897b831336e031e0161838222022f769d223
ARTHUR TEIXEIRA PERILLO <109046338+ArthurTPerillo@users.noreply.github.com>
2026-03-23 20:35:44 -0300

    Enhance client.py for user-driven conversion operations
    Refactor client.py to include user input for operations and handle multiple conversions.

--- client.py
+++ client.py
@@ -1,9 +1,33 @@
-from socket  import *
-from constCS import * #-
+from socket import *
+from constCS import *
 
 s = socket(AF_INET, SOCK_STREAM)
-s.connect((HOST, PORT)) # connect to server (block until accepted)
-s.send(str.encode('Hello, world'))  # send some data
-data = s.recv(1024)     # receive the response
-print (bytes.decode(data))            # print the result
-s.close()               # close the connection
+s.connect((HOST, PORT))
+
+print("Digite 'sair' para encerrar.")
+print("""
+Operações disponíveis:
+- c_to_f
+- f_to_c
+- km_to_miles
+- miles_to_km
+- kg_to_lb
+- lb_to_kg
+
+Formato: op1|valor
+""")
+
+while True:
+    msg = input(">>> ")
+
+    if msg.lower() == "sair":
+        s.send(str.encode("exit|"))
+        break
+
+    s.send(str.encode(msg))
+
+    data = s.recv(1024)
+
+    print("Resposta:", bytes.decode(data))
+
+s.close()
